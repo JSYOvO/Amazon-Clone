2b8dcc29a7b1d50640f7e4330e8b1de36aaa138a
JSYOvO <wotjq5113@gmail.com>
2020-10-03 12:09:14 +0900

    Add Checkout Comp & DataLayer with ReactContextAPi

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Header from './Components/Header/Header.js'
 import Home from './Components/Home/Home.js'
+import Checkout from './Components/Checkout/Checkout.js'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <Home/>
-    </div>
+    <Router>
+      <div className="App">
+        <Header/>
+        <Switch>
+          {/* Checkout Route */}
+          <Route path="/checkout">    
+            <Checkout/>
+          </Route>
+          {/* Home page Route */}
+          <Route path="/">
+            <Home/>
+          </Route>
+        </Switch>
+        
+      </div>
+    </Router>
+    
   );
 }
 
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,14 +2,21 @@ import React from 'react';
 import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import { Link } from 'react-router-dom';
+import {useStateValue} from '../../StateProvider.js';
 
 function Header() {
+    const [{basket}, dispatch] = useStateValue();
+
     return (
         <div className="header">
-            <img 
-                className="header__logo" 
-                src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" 
-            />
+            <Link to="/">
+                <img 
+                    className="header__logo" 
+                    src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" 
+                />
+            </Link>
+            
 
             <div className="header__search">
                 <input type="text" className="header__searchInput"/>
@@ -30,11 +37,13 @@ function Header() {
                     <span className="header__opeionLineTwo">Prime</span>
                 </div>
 
-                <div className="header__optionBasket">
-                    <ShoppingBasketIcon/>
-                    <span className="header__opeionLineTwo header__basketCount">0</span>
-
-                </div>
+                <Link to="/checkout">
+                    <div className="header__optionBasket">
+                        <ShoppingBasketIcon/>
+                            <span className="header__opeionLineTwo header__basketCount">{basket?.length}</span>
+                    </div>
+                </Link>
+                
             </div>
         </div>
     )
